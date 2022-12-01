cf5673a160534c526578978795912535c0070eed
cepedarod <65178917+cepedarod@users.noreply.github.com>
2022-12-01 08:23:20 -0600

    Finished day 1

---
+++ day01.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# Day 1 Puzzle
+
+#Read Input
+input_file = "input.txt"
+with open(input_file, 'r') as f:
+    lines = f.read().splitlines()
+    #print(lines)
+
+#-----------------------------------------------------
+# Part 1
+#-----------------------------------------------------
+'''
+largest_sum = 0
+current_sum = 0
+for item in lines:
+    if item: current_sum += int(item)
+    else:
+        if current_sum > largest_sum: largest_sum = current_sum
+        current_sum = 0
+
+print("largest sum: ", largest_sum)
+'''
+#-----------------------------------------------------
+# Part 2
+#-----------------------------------------------------
+sums = []
+current_sum = 0
+for item in lines:
+    if item: current_sum += int(item)
+    else: 
+        sums.append(current_sum)
+        current_sum = 0
+sums.sort(reverse=True)
+print("3 Largest Sums: ", sums[:3])
+print("Part 2 answer: ", sum(sums[:3]))
