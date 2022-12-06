74f74af9943322bca5f738311a43a65ac937c184
cepedarod <65178917+cepedarod@users.noreply.github.com>
2022-12-05 23:37:38 -0600

    removed redundant code

--- day06.py
+++ day06.py
@@ -12,30 +12,9 @@ with open(input_file, 'r') as f:
     #print(lines)
 
 input = lines[0]
-#-----------------------------------------------------
-# Part 1
-#-----------------------------------------------------
-code = []
-for answer, letter in enumerate(input):
-    if len(code) == 4:
-        print("Answer 1: ", answer)
-        break
-    while letter in code: code = code[1:]
-    code.append(letter)
 
 #-----------------------------------------------------
-# Part 2
-#-----------------------------------------------------
-code = []
-for answer, letter in enumerate(input):
-    if len(code) == 14:
-        print("Answer 2: ", answer)
-        break
-    while letter in code: code = code[1:]
-    code.append(letter)
-
-#-----------------------------------------------------
-# Bonus! Solution 2.0
+# Solution
 #-----------------------------------------------------
 def solve(target_length):
     code = []
@@ -46,6 +25,5 @@ def solve(target_length):
         while letter in code: code = code[1:]
         code.append(letter) 
 
-print("## Solution 2.0 ##")
 solve(4)
 solve(14)
