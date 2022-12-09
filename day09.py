dada2230e896120dfc7a5872f29600273d64d41d
cepedarod <65178917+cepedarod@users.noreply.github.com>
2022-12-09 16:17:34 -0600

    updated comments on day 9

--- day09.py
+++ day09.py
@@ -18,14 +18,17 @@ def chase(T, H):
     y_diff = H[1] - T[1]
     if (abs(x_diff) < 2 and abs(y_diff) < 2) or ( abs(y_diff) < 2 and abs(x_diff) < 2): return T        # No movement needed
 
+    # Ensure knot doesnt move more than one space
     if x_diff < 0: x_diff = -1
     elif x_diff > 0: x_diff = 1
     if y_diff < 0: y_diff = -1
     elif y_diff > 0: y_diff = 1
 
+    # Lateral move
     if x_diff == 0: return [T[0], T[1] + y_diff]
     elif y_diff == 0: return [T[0] + x_diff, T[1]]
-    else: return [T[0] + x_diff, T[1] + y_diff]
+
+    else: return [T[0] + x_diff, T[1] + y_diff]         # Diagonal move
 
 #-----------------------------------------------------
 # Part 1
@@ -39,7 +42,7 @@ unique_stops = ['0,0']
 for line in lines:
     direction = line.split(' ')[0]
     distance = int(line.split(' ')[1])
-    for i in range(distance):
+    for i in range(distance):                               # Head Movement
         if direction == 'U':
             H[1] += 1
         elif direction == 'D':
@@ -49,7 +52,7 @@ for line in lines:
         elif direction == 'R':
             H[0] += 1
 
-        T = chase(T, H)
+        T = chase(T, H)                                    # Tail Movement
         if f'{T[0]},{T[1]}' not in unique_stops: 
             unique_stops.append(f'{T[0]},{T[1]}')
 
@@ -67,7 +70,7 @@ for line in lines:
     direction = line.split(' ')[0]
     distance = int(line.split(' ')[1])
 
-    for i in range(distance):
+    for i in range(distance):                               # Head Movement
         if direction == 'U':
             H[1] += 1
         elif direction == 'D':
@@ -77,7 +80,7 @@ for line in lines:
         elif direction == 'R':
             H[0] += 1
         
-        knots[0] = chase(knots[0], H)
+        knots[0] = chase(knots[0], H)                      # Tails Movement
         for i in range(1, len(knots)):
             knots[i] = chase(knots[i], knots[i-1])
         
